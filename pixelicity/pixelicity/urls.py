731c6ab30105b332ec2bfbf9feb6b28ecff2f307
Megan Belzner <belzner@mit.edu>
2014-01-16 17:42:46 -0500

    Restructured CSS, added 'About' page

--- pixelicity/pixelicity/urls.py
+++ pixelicity/pixelicity/urls.py
@@ -12,5 +12,6 @@ urlpatterns = patterns('',
     # url(r'^$', 'pixelicity.views.home', name='home'),
     # url(r'^blog/', include('blog.urls')),
     url(r'^$', views.index, name='index'),
+    url(r'^about', views.about, name='about'),
     url(r'^admin/', include(admin.site.urls)),
 ) + static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
