1f515f7f34c039b6397ab4582bbc9a7433969b6e
Megan Belzner <belzner@mit.edu>
2014-01-19 15:43:43 -0500

    Added login/logout system

--- pixelicity/pixelicity/urls.py
+++ pixelicity/pixelicity/urls.py
@@ -13,5 +13,7 @@ urlpatterns = patterns('',
     # url(r'^blog/', include('blog.urls')),
     url(r'^$', views.index, name='index'),
     url(r'^about', views.about, name='about'),
-    url(r'^admin/', include(admin.site.urls)),
+    url(r'^admin', include(admin.site.urls)),
+    url(r'^login', views.userlogin, name='login'),
+    url(r'^logout', views.userlogout, name='logout'),
 ) + static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
--- pixelicity/pixgame/views.py
+++ pixelicity/pixgame/views.py
@@ -1,8 +1,28 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
+from django.contrib.auth import authenticate, login, logout
 
 # Create your views here.
 def index(request):
-	return render(request, 'index.html')
+	name = ""
+	if request.user.is_authenticated():
+		name = request.user.first_name
+	return render(request, 'index.html', {'name': name})
 
 def about(request):
-	return render(request, 'about.html')
+	name = ""
+	if request.user.is_authenticated():
+		name = request.user.first_name
+	return render(request, 'about.html', {'name': name})
+
+def userlogin(request):
+	un = request.POST['username']
+	pw = request.POST['password']
+	user = authenticate(username=un, password=pw)
+	if user is not None:
+		if user.is_active:
+			login(request, user)
+	return redirect('index')
+
+def userlogout(request):
+	logout(request)
+	return redirect('index')
